6f862a7745ec6f62d3c5c7b7e6bcc2943ed82661
Your Name <mohannadabdelnassereltaib@gmail.com>
2026-05-07 17:54:50 +0300

    ربط Firebase مع الاحتفاظ بمجلد mohamed_work

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:new_version/screens/splash_screen.dart';
-import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main(){
-
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-
-      home: const SplashScreen()));
-
+    debugShowCheckedModeBanner: false,
+    home: const SplashScreen(),
+  ));
 }
