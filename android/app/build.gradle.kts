c28b4c302493cc382c752a1e41c22a9056e5a40b
Khaled-Alii <khaledqlach@gmail.com>
2026-06-22 08:42:29 +0300

    Add internet connection check before auth requests

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "com.example.new_version"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
