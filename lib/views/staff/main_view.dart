dd821fecbee409fb955a00ae2ff37c398882c5f2
omar <omar1ali2k3@gmail.com>
2026-06-27 01:16:44 +0300

    Update main_view

--- lib/views/staff/main_view.dart
+++ lib/views/staff/main_view.dart
@@ -4,9 +4,12 @@ import 'package:google_fonts/google_fonts.dart';
 
 import 'package:new_version/controllers/home/nav_controller.dart';
 import 'package:new_version/controllers/staff_controller.dart';
+import 'package:new_version/views/staff/home/crowd_times_view.dart';
 import 'package:new_version/views/staff/home/staff_home_view.dart';
 import 'package:new_version/views/staff/profile/profile_view.dart';
 import 'package:new_version/widgets/common/custom_bottom_nav.dart';
+
+import 'reports/staff_reports_view.dart';
 
 class MainView extends StatelessWidget {
   MainView({super.key}) {
@@ -23,9 +26,9 @@ class MainView extends StatelessWidget {
           case 0:
             return StaffHomeView();
           case 1:
-            return Center(child: Text("التقارير", style: GoogleFonts.cairo()));
+            return StaffReportsView();
           case 2:
-            return Center(child: Text("السجل", style: GoogleFonts.cairo()));
+            return CrowdTimesView();
           case 3:
             return const ProfileView();
           default:
